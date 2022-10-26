5149f2c602e75fdc9eb1aea72317a9d0768b01cf
Salemmander <salemnassar5@gmail.com>
2022-10-26 09:51:00 -0500

    Created ClubHub Xcode Project

---
+++ ClubHub/ClubHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClubHub
+//
+//  Created by Salem Nassar on 10/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
