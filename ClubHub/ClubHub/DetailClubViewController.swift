5531e6ebe45ff8de245cb942c9e82d0c5b0aad32
marcenugo1 <marce.nugo1@gmail.com>
2022-10-27 14:16:13 -0500

    Users can view details of each club by tapping on them
    Users can view details of each club by tapping on them

--- ClubHub/ClubHub/DetailClubViewController.swift
+++ ClubHub/ClubHub/DetailClubViewController.swift
@@ -12,12 +12,27 @@ class DetailClubViewController: UIViewController {
     
     var club: PFObject!
 
+    @IBOutlet weak var logoView: UIImageView!
+    @IBOutlet weak var clubNameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
         print(club["clubName"] as! String)
+        
+        clubNameLabel.text = club["clubName"] as? String
+        descriptionLabel.text = club["description"] as? String
+        emailLabel.text = club["email"] as? String
+        
+        let imageFile = club["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string:urlString)!
+        
+        logoView.af.setImage(withURL: url)
+     
     }
     
 
